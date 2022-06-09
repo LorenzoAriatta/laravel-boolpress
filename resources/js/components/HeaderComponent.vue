ee784fd6016498db2e29ddb6608137de8b9f8add
Lorenzo Ariatta <lorenzotk77@gmail.com>
2022-06-09 16:04:25 +0200

    add data for dynamic menu and add v-for to generate menuItems

---
+++ resources/js/components/HeaderComponent.vue
@@ -0,0 +1,30 @@
+<template>
+  <nav class="navbar navbar-light bg-white d-flex justify-content-center py-3">
+    <ul class="nav">
+      <li v-for="(menuItem, index) in menu" :key="index" class="nav-item mr-3">
+        <router-link :to="{ name: menuItem.linkName }">{{
+          menuItem.label
+        }}</router-link>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "HeaderComponent",
+  data() {
+    return {
+      menu: [
+        { linkName: "home", label: "Home" },
+        { linkName: "blog", label: "My Posts" },
+        { linkName: "who-we-are", label: "Who We Are" },
+        { linkName: "contacts", label: "Contacts" },
+      ],
+    };
+  },
+};
+</script>
+
+<style>
+</style>
