d11a16bfdb554c1eb723d655c7f4c1ed0a647a4b
Lorenzo Ariatta <lorenzotk77@gmail.com>
2022-06-09 16:14:26 +0200

    add axios call to show data with PostCardListComponent by props

--- resources/js/pages/BlogComponent.vue
+++ resources/js/pages/BlogComponent.vue
@@ -4,6 +4,11 @@
       <div class="col-12 text-center">
         <h1 class="mt-5">My Posts</h1>
       </div>
+      <div v-if="posts.length > 0">
+        <!-- se l'array contiene dei dati andiamo visualizzarli con il v-if -->
+        <PostCardListComponent :posts="posts" />
+      </div>
+      <div v-else>Loading Posts</div>
     </div>
   </div>
 </template>
@@ -11,6 +16,25 @@
 <script>
 export default {
   name: "BlogComponent",
+  data() {
+    return {
+      posts: [],
+    };
+  },
+  mounte() {
+    window.axios
+      .get("http://127.0.0.1:8000/api/posts")
+      .then(({ status, data }) => {
+        console.log(data);
+        //effettuo un controllo su status
+        if (status === 200 && results.data.success) {
+          this.posts = results.data.results;
+        }
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  },
 };
 </script>
 
