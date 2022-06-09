81389537849388efd865e65467db2ce276d10fec
Lorenzo Ariatta <lorenzotk77@gmail.com>
2022-06-09 15:22:11 +0200

    set name in export

--- resources/js/app/AppComponent.vue
+++ resources/js/app/AppComponent.vue
@@ -1,10 +1,10 @@
 <template>
-  <div>Home</div>
+  <router-view></router-view>
 </template>
 
 <script>
 export default {
-  name: "HomeComponent",
+  name: "AppComponent",
 };
 </script>
 
---
+++ resources/js/pages/ContactsComponent.vue
@@ -0,0 +1,10 @@
+<template></template>
+
+<script>
+export default {
+  name: "ContactsComponent",
+};
+</script>
+
+<style>
+</style>
